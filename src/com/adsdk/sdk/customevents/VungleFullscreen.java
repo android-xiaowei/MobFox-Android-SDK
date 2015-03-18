1cb5dbaea698cb7cfe77fd63e0da6cf84b761c3b
Michał Kapuściński <michal.kapuscinski@gecko-lab.com>
2015-03-18 13:48:47 +0100

    Use reflection in Vungle fullscreen custom event.

--- src/com/adsdk/sdk/customevents/VungleFullscreen.java
+++ src/com/adsdk/sdk/customevents/VungleFullscreen.java
@@ -1,16 +1,22 @@
-
 package com.adsdk.sdk.customevents;
 
-import com.vungle.publisher.EventListener;
-import com.vungle.publisher.VunglePub;
+import java.lang.reflect.Array;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Handler;
 
-//Uses Vungle SDK 3.2.0
+//Based on Vungle SDK 3.3.0
 public class VungleFullscreen extends CustomEventFullscreen {
 
-	private VunglePub vunglePub;
+	private Object vunglePub;
+	private Object eventListenerArray;
+	private Class<?> vungleClass;
+	private Class<?> listenerClass;
+
 	private boolean alreadyReportedAdLoadStatus;
 
 	@Override
@@ -21,99 +27,149 @@ public class VungleFullscreen extends CustomEventFullscreen {
 		this.trackingPixel = trackingPixel;
 
 		try {
-			Class.forName("com.vungle.publisher.EventListener");
-			Class.forName("com.vungle.publisher.VunglePub");
+			listenerClass = Class.forName("com.vungle.publisher.EventListener");
+			vungleClass = Class.forName("com.vungle.publisher.VunglePub");
 		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
 			if (listener != null) {
 				listener.onFullscreenFailed();
 			}
 			return;
 		}
-		vunglePub = VunglePub.getInstance();
-		vunglePub.init(activity, adId);
-		vunglePub.setEventListener(createListener());
-		if (vunglePub.isCachedAdAvailable()) {
-			if (listener != null) {
-				listener.onFullscreenLoaded(this);
-			}
-		} else {
-			Handler h = new Handler();
-			h.postDelayed(new Runnable() {
 
-				@Override
-				public void run() {
-					if (vunglePub.isCachedAdAvailable()) {
-						if (listener != null && !alreadyReportedAdLoadStatus) {
-							listener.onFullscreenLoaded(VungleFullscreen.this);
-							alreadyReportedAdLoadStatus = true;
-						}
-					} else {
-						if (listener != null && !alreadyReportedAdLoadStatus) {
-							listener.onFullscreenFailed();
-							alreadyReportedAdLoadStatus = true;
-						}
-					}
+		try {
 
+			Method getInstanceMethod = vungleClass.getMethod("getInstance");
+			vunglePub = getInstanceMethod.invoke(null);
+
+			Method initMethod = vungleClass.getMethod("init", new Class[] { Context.class, String.class });
+			initMethod.invoke(vunglePub, activity, adId);
+
+			Object eventListener = createListener();
+			eventListenerArray = Array.newInstance(listenerClass, 1);
+			Array.set(eventListenerArray, 0, eventListener);
+			Method addEventListenersMethod = vungleClass.getMethod("addEventListeners", eventListenerArray.getClass());
+
+			addEventListenersMethod.invoke(vunglePub, eventListenerArray);
+
+			final Method isAdPlayableMethod = vungleClass.getMethod("isAdPlayable");
+
+			if ((Boolean) isAdPlayableMethod.invoke(vunglePub)) {
+				if (listener != null) {
+					listener.onFullscreenLoaded(this);
 				}
-			}, 5000);
+			} else {
+				Handler h = new Handler();
+				h.postDelayed(new Runnable() {
+
+					@Override
+					public void run() {
+						try {
+							if ((Boolean) isAdPlayableMethod.invoke(vunglePub)) {
+								if (listener != null && !alreadyReportedAdLoadStatus) {
+									listener.onFullscreenLoaded(VungleFullscreen.this);
+									alreadyReportedAdLoadStatus = true;
+								}
+							} else {
+								if (listener != null && !alreadyReportedAdLoadStatus) {
+									listener.onFullscreenFailed();
+									alreadyReportedAdLoadStatus = true;
+								}
+							}
+						} catch (Exception e) {
+							e.printStackTrace();
+							if (listener != null && !alreadyReportedAdLoadStatus) {
+								listener.onFullscreenFailed();
+								alreadyReportedAdLoadStatus = true;
+							}
+						}
+
+					}
+				}, 5000);
+			}
+
+		} catch (Exception e) {
+			if (listener != null) {
+				listener.onFullscreenFailed();
+			}
 		}
 
 	}
 
-	private EventListener createListener() {
-		return new EventListener() {
+	private Object createListener() {
+		Object instance = Proxy.newProxyInstance(listenerClass.getClassLoader(), new Class<?>[] { listenerClass }, new InvocationHandler() {
 
 			@Override
-			public void onAdEnd(boolean wasAdClicked) {
-				if (listener != null) {
-					if(wasAdClicked) {
-						listener.onFullscreenLeftApplication();
+			public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+				if (method.getName().equals("onAdEnd")) {
+					if (listener != null && args[0] != null) {
+						if ((Boolean) args[0]) {
+							listener.onFullscreenLeftApplication();
+						}
+						listener.onFullscreenClosed();
 					}
-					listener.onFullscreenClosed();
+				} else if (method.getName().equals("onAdStart")) {
+					reportImpression();
+					if (listener != null) {
+						listener.onFullscreenOpened();
+					}
+				} else if (method.getName().equals("onAdUnavailable")) {
+					if (listener != null && !alreadyReportedAdLoadStatus) {
+						listener.onFullscreenFailed();
+						alreadyReportedAdLoadStatus = true;
+					}
+				} else if (method.getName().equals("onAdPlayableChanged")) {
+					if (listener != null && !alreadyReportedAdLoadStatus && args[0] != null && (Boolean) args[0]) {
+						listener.onFullscreenLoaded(VungleFullscreen.this);
+						alreadyReportedAdLoadStatus = true;
+					}
+				} else if (method.getName().equals("hashCode")) {
+					return hashCode();
+				} else if (method.getName().equals("equals")) {
+					return equals(args[0]);
 				}
+				return null;
 			}
 
-			@Override
-			public void onAdStart() {
-				reportImpression();
-				if (listener != null) {
-					listener.onFullscreenOpened();
-				}
-			}
+		});
 
-			@Override
-			public void onAdUnavailable(String arg0) {
-				if (listener != null && !alreadyReportedAdLoadStatus) {
-					listener.onFullscreenFailed();
-					alreadyReportedAdLoadStatus = true;
-				}
-			}
+		return instance;
 
-			@Override
-			public void onCachedAdAvailable() {
-				if (listener != null && !alreadyReportedAdLoadStatus) {
-					listener.onFullscreenLoaded(VungleFullscreen.this);
-					alreadyReportedAdLoadStatus = true;
-				}
-			}
-
-			@Override
-			public void onVideoView(boolean arg0, int arg1, int arg2) {
-			}
-
-		};
 	}
 
 	@Override
 	public void finish() {
 		super.finish();
-		vunglePub.setEventListener(null);
+		if (vunglePub != null && vungleClass != null) {
+			try {
+				Method onPauseMethod = vungleClass.getMethod("onPause");
+				onPauseMethod.invoke(vunglePub);
+				if (eventListenerArray != null) {
+					Method removeEventListenersMethod = vungleClass.getMethod("removeEventListeners", eventListenerArray.getClass());
+					removeEventListenersMethod.invoke(vunglePub, eventListenerArray);
+				}
+			} catch (Exception e) {
+			}
+		}
+		eventListenerArray = null;
+		vunglePub = null;
 	}
 
 	@Override
 	public void showFullscreen() {
-		if (vunglePub != null && vunglePub.isCachedAdAvailable()) {
-			vunglePub.playAd();
+		if (vunglePub != null && vungleClass != null) {
+			try {
+				final Method isAdPlayableMethod = vungleClass.getMethod("isAdPlayable");
+				if ((Boolean) isAdPlayableMethod.invoke(vunglePub)) {
+					Method playAdMethod = vungleClass.getMethod("playAd");
+					playAdMethod.invoke(vunglePub);
+				}
+			} catch (Exception e) {
+				if (listener != null) {
+					listener.onFullscreenFailed();
+				}
+			}
 		}
 
 	}
