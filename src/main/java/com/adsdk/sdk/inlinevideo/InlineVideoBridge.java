018f0cd1adf2cdd14211c682104529d31940f7bc
Itamar Nabriski <itamar.n@matomy.com>
2015-07-28 14:48:10 +0300

    fix the closing/opening in inline video

---
+++ src/main/java/com/adsdk/sdk/inlinevideo/InlineVideoBridge.java
@@ -0,0 +1,76 @@
+package com.adsdk.sdk.inlinevideo;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+/**
+ * Created by nabriski on 7/28/15.
+ */
+public class InlineVideoBridge {
+
+
+   public interface Listener{
+
+       public void onAdClicked(String href);
+
+       public void onAdClosed();
+
+       public void onAdFinished();
+
+       public void onAdError(Exception e);
+
+       public void onAdLoaded();
+
+   }
+
+   Context ctx;
+   Listener listener;
+
+
+   public InlineVideoBridge(Context ctx,Listener listener){
+        this.ctx = ctx;
+        this.listener = listener;
+   }
+
+    public void send(String json) {
+
+        JSONObject action = null;
+        try {
+            action = new JSONObject(json);
+            JSONArray keys = action.names();
+            String key = keys.get(0).toString();
+            String value = action.get(key).toString();
+
+            if (key.equals("adLoaded")) {
+                listener.onAdLoaded();
+            }
+            else if (key.equals("clickURL")) {
+               listener.onAdClicked(value);
+            }
+
+            else if (key.equals("close")) {
+                listener.onAdClosed();
+            }
+
+            else if (key.equals("finished")) {
+                listener.onAdFinished();
+            }
+
+            else if(key.equals("error")){
+                listener.onAdError(new Exception(value));
+            }
+            else{ //unknown key treat as error
+                listener.onAdError(new Exception(value));
+            }
+
+        } catch (JSONException e) {
+            listener.onAdError(e);
+        }
+
+    }
+}
--- src/main/java/com/adsdk/sdk/inlinevideo/InlineVideoView.java
+++ src/main/java/com/adsdk/sdk/inlinevideo/InlineVideoView.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Handler;
 import android.util.AttributeSet;
 
 import android.view.View;
@@ -31,68 +32,32 @@ import com.adsdk.sdk.nativeformats.Utils;
 /**
  * Created by itamar on 19/03/15.
  */
-public class InlineVideoView extends VideoEnabledWebView {
+public class InlineVideoView extends VideoEnabledWebView implements InlineVideoBridge.Listener{
 
-    private static final String BASE_URL = "http://my.mobfox.com/request.php";
+    static final String BASE_URL = "http://my.mobfox.com/request.php";
 
-    private String publicationId;
-    int adWidth = 0;
-    int adHeight = 0;
+    public interface Listener{
 
-    final Context ctx = this.getContext();
+        public void onAdClicked(String href);
 
-    NativeFormatRequest nfr = new NativeFormatRequest();
+        public void onAdClosed();
 
-    public class BridgeInterface {
-        Context mContext;
+        public void onAdFinished();
 
-        /** Instantiate the interface and set the context */
-        BridgeInterface(Context c) {
-            mContext = c;
-        }
+        public void onAdError(Exception e);
 
-        @SuppressLint("JavascriptInterface")
-        public void send(String json) {
-
-            try {
-                JSONObject action = new JSONObject(json);
-                JSONArray keys = action.names();
-                String key = keys.get(0).toString();
-                String value = action.get(key).toString();
-
-                if (key.equals("clickURL")) {
-                    adClick(value, key);
-                }
-
-                if (key.equals("close")) {
-                    adClose(value, key);
-                }
-
-                if (key.equals("finished")) {
-                    adFinish(value, key);
-                }
-
-//                if (key.equals("fullscreen")) {
-//                    toggleFullscreen(value);
-//                }
-
-                if (key.equals("error")) {
-                    adError(value, key);
-                }
-
-                if (key.equals("adLoaded")) {
-                    adLoaded(value, key);
-                }
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-        }
+        public void onAdLoaded();
 
     }
 
-    public String getSrc(String data, String fileName) {
+    String publicationId;
+    int adWidth = 0;
+    int adHeight = 0;
+    Listener listener;
+    boolean autoplay =true,skip =true;
+    Handler handler;
+
+    protected String getSrc(String data, String fileName) {
 
         String tDir = System.getProperty("java.io.tmpdir");
 
@@ -110,13 +75,15 @@ public class InlineVideoView extends VideoEnabledWebView {
 
     }
 
-    @SuppressLint({ "NewApi", "SetJavaScriptEnabled" })
-    private void init() {
+    @SuppressLint("JavascriptInterface")
+    protected void init() {
 
+        handler = new Handler();
         WebSettings webSettings = getSettings();
         webSettings.setJavaScriptEnabled(true);
 
-        this.addJavascriptInterface(new BridgeInterface(this.getContext()), "Android");
+        InlineVideoBridge bridge = new InlineVideoBridge(getContext(),this);
+        this.addJavascriptInterface(bridge, "Android");
 
         if (android.os.Build.VERSION.SDK_INT >= 16) {
 
@@ -154,60 +121,26 @@ public class InlineVideoView extends VideoEnabledWebView {
         init();
     }
 
+
     public void setPublicationId(String id) {
         this.publicationId = id;
     }
 
-    private void adClick(String value, String key) {
-
-        //video clicked
-        android.util.Log.d("userAction", "key: " + key + ", value: " + value);
-
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(value));
-        getContext().startActivity(intent);
-
+    public void setListener(Listener listener) {
+        this.listener = listener;
     }
 
-    private void adClose(String value, String key) {
-
-        //video closed
-        android.util.Log.d("userAction", "key: " + key + ", value: " + value);
-
-        setVisibility(View.GONE);
-
+    public void setAutoplay(boolean autoplay) {
+        this.autoplay = autoplay;
     }
 
-    private void adFinish(String value, String key) {
-
-        //video finsihed
-        android.util.Log.d("userAction", "key: " + key + ", value: " + value);
-
-    }
-
-//    private void toggleFullscreen(String value, String key) {
-//
-//        //toggle fullscreen
-//        android.util.Log.d("userAction", "key: " + key + ", value: " + value);
-//
-//    }
-
-    private void adError(String value, String key) {
-
-        //loadAd error
-        android.util.Log.d("userAction", "key: " + key + ", value: " + value);
-
-    }
-
-    private void adLoaded(String value, String key) {
-
-        //ad Loaded
-        android.util.Log.d("userAction", "key: " + key + ", value: " + value);
-
-        setVisibility(View.VISIBLE);
-
+    public void setSkip(boolean skip) {
+        this.skip = skip;
     }
 
     public void loadAdInternal(){
+
+        clearView();
 
         setWebViewClient(new WebViewClient() {
 
@@ -216,49 +149,22 @@ public class InlineVideoView extends VideoEnabledWebView {
 
                 setWebViewClient(null);
 
-                JSONObject params = new JSONObject();
-
                 String ipAddress = Utils.getIPAddress(); //TODO: can we remove it? Other requests don't send IP
                 if (ipAddress.indexOf("10.") == 0 || ipAddress.length() == 0) {
                     ipAddress = "2.122.29.194";
                 }
 
-                String userAgent = Util.getDefaultUserAgentString(ctx);
+                String userAgent = Util.getDefaultUserAgentString(getContext());
                 String o_andadvid = Util.getAndroidAdId();
-                Boolean autoplay = true;
-                Boolean skip = true;
+
+                JSONObject params = new JSONObject();
 
                 try {
                     params.put("s", publicationId);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                try {
                     params.put("u", userAgent);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                try {
                     params.put("i", ipAddress);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                try {
                     params.put("o_andadvid", o_andadvid);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                try {
                     params.put("autoplay", autoplay);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                try {
                     params.put("skip", skip);
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -280,6 +186,7 @@ public class InlineVideoView extends VideoEnabledWebView {
         String srcPath = getSrc(inlineVideoString, inlineName);
 
         loadDataWithBaseURL(srcPath, inlineVideo, "text/html", "utf-8", null);
+
 
     }
 
@@ -321,4 +228,44 @@ public class InlineVideoView extends VideoEnabledWebView {
 
     }
 
+    @Override
+    public void onAdClicked(String href) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(href));
+        getContext().startActivity(intent);
+        if(listener!=null) listener.onAdClicked(href);
+    }
+
+    @Override
+    public void onAdClosed() {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                setVisibility(View.GONE);
+                if(listener!=null) listener.onAdClosed();
+            }
+        });
+    }
+
+    @Override
+    public void onAdFinished() {
+        if(listener!=null) listener.onAdFinished();
+    }
+
+    @Override
+    public void onAdError(Exception e) {
+        if(listener!=null) listener.onAdError(e);
+    }
+
+    @Override
+    public void onAdLoaded() {
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                setVisibility(View.VISIBLE);
+                if(listener!=null) listener.onAdLoaded();
+            }
+        });
+
+    }
 }
