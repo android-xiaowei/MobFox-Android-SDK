f1a811a981ec5d656b774bc0b36141e26a64aedf
Itamar Nabriski <itamar.n@matomy.com>
2015-09-06 14:46:25 +0300

    fix package script

--- scripts/create_dist.sh
+++ scripts/create_dist.sh
@@ -19,12 +19,16 @@ mv $TEMP/release-{{ver}}-android $TEMP/release-$VER-android
 DIR=$TEMP/release-$VER-android
 mv $DIR/MobFox-Android-SDK-{{ver}} $DIR/MobFox-Android-SDK-$VER
 mv $DIR/MobFox_Android_SDK+Mediation/Readme_GoogleAdMobAdapter_MobFoxSDK_Android_{{ver}}.txt $DIR/MobFox_Android_SDK+Mediation/Readme_GoogleAdMobAdapter_MobFoxSDK_Android_$VER.txt
-cp ../build/outputs/MobFox-Android-SDK.jar $DIR/MobFox-Android-SDK-$VER/AdSdk_$VER.jar
+cp ../build/outputs/MobFox-Android-SDK-$VER.jar $DIR/MobFox-Android-SDK-$VER/AdSdk_$VER.jar
 
 cp $DIR/MobFox-Android-SDK-$VER/AdSdk_$VER.jar $DIR/MobFox-Android-SDK-$VER/AdSDK_Demo/libs/
 cp $DIR/MobFox-Android-SDK-$VER/AdSdk_$VER.jar $DIR/MobFox-Android-SDK-$VER/AdSDK_NativeAdsDemo/libs/
 cp $DIR/MobFox-Android-SDK-$VER/AdSdk_$VER.jar $DIR/MobFox-Android-SDK-$VER/AdSDK_XML_Demo/libs/
 
 cp ../out/artifacts/Mobfox_Android_SDK/Mobfox-Android-SDK.jar $DIR/MobFox_Android_SDK+Mediation/AdSdk_$VER.jar
-zip -r $OUT/MobFox-Android-SDK-$VER.zip $DIR
 
+pushd $TEMP
+zip -r MobFox-Android-SDK-$VER.zip release-$VER-android
+popd
+
+mv $TEMP/MobFox-Android-SDK-$VER.zip $OUT/
