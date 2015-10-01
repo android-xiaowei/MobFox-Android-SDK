b6ae41a00b21cd6901622bcd2f54e3486a28b2f8
Asaf Gaber <asaf.g@matomy.com>
2015-10-01 16:16:45 +0300

    commit

--- src/main/java/com/adsdk/sdk/Const.java
+++ src/main/java/com/adsdk/sdk/Const.java
@@ -5,7 +5,7 @@ public interface Const {
 	public static final String ENCODING = "UTF-8";
 	public static final String RESPONSE_ENCODING = "ISO-8859-1";
 
-	public static final String VERSION = "6.2.5";
+	public static final String VERSION = "7";
 
 	public static final String PROTOCOL_VERSION = "3.0";
 
