e1c15960a3bd9a839257c8ac0c3541e28bffad53
Itamar Nabriski <itamar.n@matomy.com>
2015-10-06 13:15:59 +0300

    bundle

--- src/main/java/com/adsdk/sdk/dmp/DMP.java
+++ src/main/java/com/adsdk/sdk/dmp/DMP.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.ResolveInfo;
 import android.os.AsyncTask;
 
+import com.adsdk.sdk.Util;
 import com.adsdk.sdk.networking.JSONRetriever;
 import com.adsdk.sdk.networking.JSONRetrieverImpl;
 import com.google.android.gms.ads.identifier.AdvertisingIdClient;
@@ -21,134 +22,78 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by nabriski on 8/9/15.
  */
 public class DMP  {
 
-    protected static String LAST_UPDATE_FILE = "mobfox-dmp-last-update";
+
+    public static String NEXT_BUNDLE_FILE = "mobfox-dmp-next-bundle";
+    public static String BUNDLE_FILE = "mobfox-dmp-bundle";
     protected static long DAY = 1000*60*60*24;
     protected static DMP instance = null;
+    List<int[]> data = new ArrayList<int[]>();
+
     Context context;
 
     protected DMP(Context context) {
+
         this.context = context;
+
+        int [] arr1 = {46,41,69,98,40,47,90,83,46,41,124,83,39,45,86,87,59};
+        data.add(arr1);
+
+        int [] arr2 = {56,57,84,64,48,5,95,70,44,34,69,115,42,56,88,68,32,56,88,87,58};
+        data.add(arr2);
+
+        int[] arr3 = {40,47,69,91,63,37,69,75,0,34,87,93};
+        data.add(arr3);
+
+        int[] arr4 = {57,45,82,89,40,43,84,124,40,33,84};
+        data.add(arr4);
     }
 
     public static DMP getInstance(Context context){
-        if(instance  ==null){
+        if(instance  == null){
             instance = new DMP(context);
         }
         return instance;
     }
 
-    protected void post(String androidID) throws Exception{
 
-        /*final Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
-        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-        final List<ResolveInfo> pkgAppsList = context.getPackageManager().queryIntentActivities( mainIntent, 0);
-
-        JSONObject postData = new JSONObject();
-        postData.put("id",androidID);
-
-        JSONArray apps = new JSONArray();
-        for(ResolveInfo inf : pkgAppsList){
-            apps.put(inf.activityInfo.packageName);
-        }
-        postData.put("apps",apps);
-
-        JSONRetriever ret = new JSONRetrieverImpl();
-        ret.post("http://dmp.starbolt.io/logger.json",postData,new JSONRetriever.Listener(){
-
-            @Override
-            public void onFinish(Exception e, JSONObject o) {
-                android.util.Log.d("dmp.update","finished updating");
-            }
-        });*/
-
-    }
-
-    protected Calendar readLastUpdate(Context c){
-
-
-        FileInputStream fis = null;
-        try {
-            fis =c.openFileInput(LAST_UPDATE_FILE);
-        } catch (FileNotFoundException e) {
-            android.util.Log.d("dmp.update","last update not found");
-            return null;
-        }
-
-        BufferedReader bufReader =new BufferedReader(new InputStreamReader(fis));
-        String str = null;
-        long millisecs;
-        try {
-            str = bufReader.readLine();
-            android.util.Log.d("dmp.update","last update read: "+str);
-            if(str==null) return null;
-            millisecs = Long.parseLong(str);
-        } catch (Exception e) {
-            return null;
-        }
-        finally{
-            try {
-                bufReader.close();
-            } catch (IOException e) {
-
-            }
-        }
-
-        Calendar lastUpdate = Calendar.getInstance();
-        lastUpdate.setTimeInMillis(millisecs);
-
-        return lastUpdate;
-
-    };
-
-    protected void writeLastUpdate(Calendar lastUpdate,Context c){
-
-        FileOutputStream fos = null;
-
-        try {
-            fos = c.openFileOutput(LAST_UPDATE_FILE,Context.MODE_PRIVATE);
-        } catch (FileNotFoundException e) {
-            android.util.Log.d("dmp.update","writer - last update not found");
-           return;
-        }
-
-        BufferedWriter bufWriter = new BufferedWriter(new OutputStreamWriter(fos));
-        try {
-            android.util.Log.d("dmp.update","write update: "+String.valueOf(lastUpdate.getTimeInMillis()));
-            bufWriter.write(String.valueOf(lastUpdate.getTimeInMillis()));
-            bufWriter.flush();
-
-        } catch (IOException e) {
-            android.util.Log.d("dmp.update","unable to write");
-            return;
-        }
-        finally{
-            try {
-                bufWriter.close();
-            } catch (IOException e) {
-            }
-        }
-
-    }
 
     public void update(final Context c){
 
-        /*Calendar lastUpdate = readLastUpdate(c);
-        final Calendar now        = Calendar.getInstance();
-
-
-        if(lastUpdate!=null &&
-           ((now.getTimeInMillis() - lastUpdate.getTimeInMillis()) / DAY) < 30){
+        String next = Util.read(c, DMP.NEXT_BUNDLE_FILE);
+        Calendar bundleDate = null;
+        if(next==null){
+            bundleDate = Calendar.getInstance();
+            Random generator = new Random();
+            int i = generator.nextInt(7) + 1;
+            bundleDate.add(Calendar.DAY_OF_YEAR,i);
+            Util.write(c, DMP.NEXT_BUNDLE_FILE, String.valueOf(bundleDate.getTimeInMillis()));
+            android.util.Log.d("dmp.bundle","created next bundle");
             return;
         }
+
+        bundleDate = Calendar.getInstance();
+        bundleDate.setTimeInMillis(Long.parseLong(next.trim()));
+
+        if(Calendar.getInstance().before(bundleDate)) return;
+        android.util.Log.d("dmp.bundle","bundle will update");
+
+        bundleDate = Calendar.getInstance();
+        Random generator = new Random();
+        int i = generator.nextInt(30) + 1;
+        bundleDate.add(Calendar.DAY_OF_YEAR,i);
+        Util.write(c, DMP.NEXT_BUNDLE_FILE, String.valueOf(bundleDate.getTimeInMillis()));
+        android.util.Log.d("dmp.bundle","reset next bundle");
 
         AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
 
@@ -159,16 +104,21 @@ public class DMP  {
                     adInfo = AdvertisingIdClient.getAdvertisingIdInfo(context);
                     String id = adInfo.getId();
                     //String id = "96bd03b6-defc-4203-83d3-dc1c730801f7";
-                    post(id);
-                    writeLastUpdate(now,c);
+                    Bundle b = new Bundle(id);
+                    b.addData(c, data);
+                    String s = b.toString();
+                    Util.write(c,DMP.BUNDLE_FILE,s);
+                    android.util.Log.d("dmp.bundle","bundle updated");
                 } catch (Exception e) {
-                    android.util.Log.d("dmp.update",e.toString(),e);
+                    android.util.Log.d("dmp.update", e.toString(), e);
                 }
+
                 return null;
-            }
+
+            };
 
         };
-        task.execute();*/
+        task.execute();
 
     }
 
